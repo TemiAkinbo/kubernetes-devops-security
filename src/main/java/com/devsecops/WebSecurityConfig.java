ab7e9d1ffc4566f9131fc3e61f3ea220770f24c8
Temi <temi.akinbo01@gmail.com>
2022-11-08 11:16:50 +0000

    websecurityconfig

--- src/main/java/com/devsecops/WebSecurityConfig.java
+++ src/main/java/com/devsecops/WebSecurityConfig.java
@@ -10,6 +10,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
     }
 }
